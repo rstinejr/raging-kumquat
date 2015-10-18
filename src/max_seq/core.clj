a815e5421397c17b318d57accd8d14068ba8bd19
R.H. Stine <bob@waltonstine.net>
2015-10-18 10:00:00 -0400

    have main run explicit and internal memoized functions.

--- src/max_seq/core.clj
+++ src/max_seq/core.clj
@@ -5,5 +5,10 @@
 (defn -main
   "Take two strings as args."
   [& args]
-  (let [comn (lcs/explicit-memo (first args) (second args))]
-    (println (str "max-common of '" (first args) "' and '" (second args) "': " comn))))
+  (if (not= 2 (count args))
+    (println "Usage: lcs <string 1> <string 2>")
+    (let [s1 (first  args)
+          s2 (second args)]
+      (println (str "LCS of '" s1 "' and '" s2 "':"))
+      (println (str "explicit memoization: " (lcs/explicit-memo s1 s2)))
+      (println (str "memoized            : " (lcs/memoized s1 s2))))))
