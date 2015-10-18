003e9856b024e11a34cdb49591e1bf764bfc3ca9
R.H. Stine <bob@waltonstine.net>
2015-10-17 20:42:01 -0400

    Stub out max-common.

--- src/max_seq/core.clj
+++ src/max_seq/core.clj
@@ -1,7 +1,23 @@
 (ns max-seq.core
   (:gen-class))
 
+(def common-seqs (atom {}))
+
+(def seq1 (atom []))
+(def seq2 (atom []))
+
+
+(defn max-common
+  [arg1 arg2]
+  (reset! seq1 (vec arg1))
+  (reset! seq2 (vec arg2))
+  [ ])
+
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Take two strings as args."
   [& args]
-  (println "Hello, World!"))
+  (when (not= 2 (count args))
+    (throw (ex-info (str "Need exactly two sequences to compare.") {:causes #{:bad-args}})))
+  (let [seq1 (into [] (map str (seq (first  args))))
+        seq2 (into [] (map str (seq (second args))))]
+  (println (str "max-common of '" seq1 "' and '" seq2 "': " (max-common seq1 seq2)))))
