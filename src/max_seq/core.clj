bdd4d5fd027953c175ee1a66b8b493305e621576
R.H. Stine <bob@waltonstine.net>
2015-10-18 10:09:58 -0400

    Show result of no memoization.

--- src/max_seq/core.clj
+++ src/max_seq/core.clj
@@ -9,6 +9,10 @@
     (println "Usage: lcs <string 1> <string 2>")
     (let [s1 (first  args)
           s2 (second args)]
+      (println)
       (println (str "LCS of '" s1 "' and '" s2 "':"))
+      (println (str "no memoization      : " (time (lcs/no-memo       s1 s2))))
+      (println)
       (println (str "explicit memoization: " (time (lcs/explicit-memo s1 s2))))
-      (println (str "internally memoized : " (time (lcs/memoized s1 s2)))))))
+      (println)
+      (println (str "internally memoized : " (time (lcs/memoized      s1 s2)))))))
