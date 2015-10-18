3a1d0a1d62b816a37967a2aef8baadf8a489d219
R.H. Stine <bob@waltonstine.net>
2015-10-18 00:58:45 -0400

    More tests.

--- test/max_seq/core_test.clj
+++ test/max_seq/core_test.clj
@@ -2,23 +2,17 @@
   (:require [clojure.test :refer :all]
             [max-seq.core :refer :all]))
 
-(deftest test-some-matches
-;;  (testing "empty lists"
-;;    (is (= () (max-common "" "")))
-;;    (is (= () (max-common "" "abcd")))
-;;    (is (= () (max-common "defz" ""))))
-  (testing "singletons"
- ;;   (is (= () (max-common "a" "b")))
-    (let [res (max-common "b" "b")]
-      (println (str "result, \"b\" and \"b\": " res))
-      (is (= ["b"] res))))
-  ;;(testing "pairs"
-  ;;  (let [res (max-common "ab" "a")]
-  ;;    (println (str "result, \"ab\", \"a\": " res))
-  ;;    (is (= ["a"] res)))
-  ;;  (is (= ["a"] (max-common "ba" "a")))
-  ;;  (is (= ["c"] (max-common "c" "cd")))
-  ;;  (is (= ["c"] (max-common "c" "dc"))))
-    )
-  
+(deftest test-empties
+  (testing "expect empty list"
+    (is (= () (max-common "" "")))
+    (is (= () (max-common "" "abcd")))
+    (is (= () (max-common "defz" "")))
+    (is (= () (max-common "a" "b")))))
 
+(deftest short-strings
+  (testing "expect singletons"
+    (is (= ["a"] (max-common "ab" "a")))
+    (is (= ["b"] (max-common "b"  "b")))
+    (is (= ["a"] (max-common "ba" "a")))
+    (is (= ["c"] (max-common "c" "cd")))
+    (is (= ["c"] (max-common "c" "dc")))))
