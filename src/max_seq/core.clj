d47e0b5b4c3fc4e5c6800c4b6e4d7a728a11dad8
R.H. Stine <bob@waltonstine.net>
2015-10-18 00:52:41 -0400

    Pass atom to function.

--- src/max_seq/core.clj
+++ src/max_seq/core.clj
@@ -8,17 +8,30 @@
     s1
     s2))
 
+(defn- make-key
+  [i j]
+  (keyword (str i "-" j)))
+
 (defn- max-seq
-  [i j s1 s2]
-  (let [x (first (drop i s1))
-        y (first (drop j s2))]
-    (if (or (nil? x) (nil? y))
-      ()
-      (longer-seq  
-        (if (not= x y) () (concat [x] (max-seq (inc i) (inc j) s1 s2)))
-        (longer-seq
-          (max-seq i       (inc j) s1 s2)
-          (max-seq (inc i) j       s1 s2))))))
+  [i j s1 s2 cache]
+  (let [iter-key (make-key i j)
+        cached-val (iter-key @cache)]
+    (if cached-val
+      (do
+        (println (str "cach hit for " i ", " j "<<<"))
+        (flush)
+        cached-val)
+      (let [x       (first (drop i s1))
+            y       (first (drop j s2))
+            new-seq (if (or (nil? x) (nil? y))
+                      ()
+                      (longer-seq
+                        (if (not= x y) () (concat [x] (max-seq (inc i) (inc j) s1 s2 cache)))
+                        (longer-seq
+                          (max-seq i       (inc j) s1 s2 cache)
+                          (max-seq (inc i) j       s1 s2 cache))))]
+        (swap! cache assoc iter-key new-seq)
+        new-seq))))
         
 (defn max-common
   [& args]
@@ -26,7 +39,7 @@
     (throw (ex-info (str "Need exactly two sequences to compare.") {:causes #{:bad-args}})))
   (let [s1 (map str (seq (first  args)))
         s2 (map str (seq (second args)))]
-    (into [] ((memoize max-seq) 0 0 s1 s2))))
+    (into [] ((memoize max-seq) 0 0 s1 s2 (atom {})))))
 
 (defn -main
   "Take two strings as args."
