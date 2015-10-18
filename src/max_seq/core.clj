ba04b179ea78fbce0b8e478d9c2933bd3f814333
R.H. Stine <bob@waltonstine.net>
2015-10-17 21:36:51 -0400

    First draft at recursion.

--- src/max_seq/core.clj
+++ src/max_seq/core.clj
@@ -10,13 +10,31 @@
   [i j]
   (keyword (str i "-" j)))
 
+(defn longer-seq
+  [s1 s2]
+  (if (>= (count s1)(count s2))
+    s1
+    s2))
+
 (defn max-seq
   [i j]
+  (println (str "max-seq: " i ", " j))(flush)
   (let [this-key (make-key i j)]
     (if-let [this-seq (this-key @seq-cache)]
       this-seq
-      [ ])))
-
+      (let [x-i   (first (drop i @seq1))
+            y-j   (first (drop j @seq2))
+            commn (if (or (nil? x-i) (nil? y-j))
+                    ()
+                    (longer-seq  
+                      (if (not= x-i y-j) () (concat '(x-i) (max-seq (inc i) (inc j))))
+                      (longer-seq
+                        (max-seq i (inc j))
+                        (max-seq (inc i) j))))]
+        (printf (str "commn for " i ", " j ": " commn))(flush)
+        (swap! seq-cache assoc this-key commn)
+        commn))))
+        
 (defn max-common
   [arg1 arg2]
   (reset! seq1 (vec arg1))
